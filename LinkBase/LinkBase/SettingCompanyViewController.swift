8f8e601a8dc891c4279670d73ddfa954770cd9f5
ROBERT HERNANDEZ <robert.hernandez@coupa.com>
2017-03-29 15:57:22 -0700

    added favorites in tile, and rough settings

---
+++ LinkBase/LinkBase/SettingCompanyViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingCompanyViewController.swift
+//  LinkBase
+//
+//  Created by Rob Hernandez on 3/29/17.
+//  Copyright © 2017 barbrobmich. All rights reserved.
+//
+
+import UIKit
+
+class SettingCompanyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+
+
+
+}
