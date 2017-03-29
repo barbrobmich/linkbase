a1a81be480fb90c4e5f801431204cadbd213938f
ROBERT HERNANDEZ <robert.hernandez@coupa.com>
2017-03-29 15:57:22 -0700

    added favorites in tile, and rough settings

--- LinkBase/LinkBase/HomeCompanyViewController.swift
+++ LinkBase/LinkBase/HomeCompanyViewController.swift
@@ -59,7 +59,7 @@ extension HomeCompanyViewController: UICollectionViewDelegate, UICollectionViewD
         }else if section == 1{
             return self.companys.count
         }else{
-            // Third section is optional
+            // Third section is optional(Trending)
             return 0
         }
     }
